1f8d19c6ac46583a1333c739ca25f1e19e55f86a
0himu1 <0himu1@gmail.com>
2024-09-24 12:46:54 +0600

    merged with salmans code

--- src/components/leadCenter/Sidebar/Reminders.tsx
+++ src/components/leadCenter/Sidebar/Reminders.tsx
@@ -1,72 +1,72 @@
-
 import React, { useState, useEffect } from 'react';
-import { Box, Typography, TextField, Button, IconButton, Modal } from '@mui/material';
- import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+import {
+	Box,
+	Typography,
+	TextField,
+	Button,
+	IconButton,
+	Modal,
+} from '@mui/material';
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 
-
-
 interface Conversation {
-  id: string;
+	id: string;
 }
 
 interface RemindersProps {
-  conversation: Conversation | null;
+	conversation: Conversation | null;
 }
 
 const Reminders: React.FC<RemindersProps> = ({ conversation }) => {
-  const style = {
-    borderRadius: "10px",
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    boxShadow: 24,
-    p: 4,
-  };
+	const style = {
+		borderRadius: '10px',
+		position: 'absolute',
+		top: '50%',
+		left: '50%',
+		transform: 'translate(-50%, -50%)',
+		width: 400,
+		bgcolor: 'background.paper',
+		boxShadow: 24,
+		p: 4,
+	};
 
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+	const [open, setOpen] = React.useState(false);
+	const handleOpen = () => setOpen(true);
+	const handleClose = () => setOpen(false);
 
-  return (
-    <Box sx={{ marginTop: 1 }}>
-      <div>
-        <div className='flex justify-center'>
-
-          <Button onClick={handleOpen} className='w-full'>Open modal</Button>
-        </div>
-        <Modal
-          open={open}
-          onClose={handleClose}
-          aria-labelledby="modal-modal-title"
-          aria-describedby="modal-modal-description"
-        >
-          <Box sx={style}>
-            <Typography id="modal-modal-title" variant="h6" component="h2">
-              Choose Date & Time for Reminder
-            </Typography>
-            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-
-              <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <DemoContainer components={['DateTimePicker']}>
-                  <DateTimePicker label="Basic date time picker" />
-                </DemoContainer>
-              </LocalizationProvider>
-
-            </Typography>
-          </Box>
-        </Modal>
-      </div>
-    </Box>
-  );
+	return (
+		<Box sx={{ marginTop: 1 }}>
+			<div>
+				<div className="flex justify-center">
+					<Button onClick={handleOpen} className="w-full">
+						Open modal
+					</Button>
+				</div>
+				<Modal
+					open={open}
+					onClose={handleClose}
+					aria-labelledby="modal-modal-title"
+					aria-describedby="modal-modal-description"
+				>
+					<Box sx={style}>
+						<Typography id="modal-modal-title" variant="h6" component="h2">
+							Choose Date & Time for Reminder
+						</Typography>
+						<Typography id="modal-modal-description" sx={{ mt: 2 }}>
+							<LocalizationProvider dateAdapter={AdapterDayjs}>
+								<DemoContainer components={['DateTimePicker']}>
+									<DateTimePicker label="Basic date time picker" />
+								</DemoContainer>
+							</LocalizationProvider>
+						</Typography>
+					</Box>
+				</Modal>
+			</div>
+		</Box>
+	);
 };
 
 export default Reminders;
-
-
-
